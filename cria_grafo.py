20bc906e70c61f92fe24a20a3704a44f308f7c32
pedrokuchpil <pedrokuchpil@gmail.com>
2020-06-30 23:24:03 -0300

    algumas melhorias

--- cria_grafo.py
+++ cria_grafo.py
@@ -12,7 +12,7 @@ def cria_grafo(ano, dicionario):
 				G.add_edge(professor, colaborador)
 	return G
 
-lista_grafos = []
+lista_grafos_1 = []
 lista_grafos_2 = []
 lista_grafos_3 = []
 
@@ -80,23 +80,20 @@ while ano <= 2020:
 	G2 = nx.Graph(G2) #Peso 1 para cada artigo
 	G3 = nx.Graph(G3) #Proporcional à afinidade
 	for professor in professores_dict:
-		G1.add_node(professor,color=colors[professor])
-		G2.add_node(professor,color=colors[professor])
-		G3.add_node(professor,color=colors[professor])
 		for colaborador in [*professores_dict[professor]]: #retorna as chaves dentro do dicionario do professor. No caso, os colaboradores
 			count = 0
 			dinamic_count = 0
 			for i in range(len(professores_dict[professor][colaborador])):
 				if (professores_dict[professor][colaborador][i]['year'] == str(ano)):
 					G1.add_edge(professor, colaborador)
-				elif(int(professores_dict[professor][colaborador][i]['year']) <= ano):
+				#elif(int(professores_dict[professor][colaborador][i]['year']) <= ano):
 					count+=1
 					dinamic_count+= 1/professores_dict[professor][colaborador][i]['number_coauthors']
 					
 			if(count>0):
 				G2.add_edge(professor, colaborador,weight=count)
 				G3.add_edge(professor, colaborador,weight=dinamic_count)
-	lista_grafos.append(G1)
+	lista_grafos_1.append(G1)
 	lista_grafos_2.append(G2)
 	lista_grafos_3.append(G3)
 	edges = G2.edges()
@@ -104,7 +101,7 @@ while ano <= 2020:
 	weights = [G2[u][v]['weight']*10 for u,v in edges]
 	ano += 1 
 
-print (len(lista_grafos))
+print (len(lista_grafos_1))
 def closureBecauseNX(G):
 	Graph = G
 	def filterNoNeighbours(V):
@@ -114,15 +111,15 @@ def closureBecauseNX(G):
 			return False
 	return filterNoNeighbours
 
-i=0
-list_number = int(input('Qual grafo ? 0,1,2?'))
-if(list_number==0):
-	real_graph_list = lista_grafos
-elif(list_number == 1):
+list_number = int(input('Qual grafo ? 1,2,3?'))
+if(list_number==1):
+	real_graph_list = lista_grafos_1
+elif(list_number == 2):
 	real_graph_list = lista_grafos_2	
 else:
 	real_graph_list = lista_grafos_3
 
+i=0
 while i<=30:
 	print (i+1990)
 	cfunction = closureBecauseNX(real_graph_list[i])
@@ -130,17 +127,16 @@ while i<=30:
 	sub_graph= nx.subgraph_view(real_graph_list[i],filter_node=cfunction)
 	edges = sub_graph.edges()
 
-	if (list_number>0):
+	if (list_number>1):
 		weights = [sub_graph[u][v]['weight'] for u,v in edges]
 		nx.draw_spring(nx.subgraph_view(real_graph_list[i],filter_node=cfunction),node_color=graph_colors,with_labels=True,node_size=500,font_size=10,width=weights)
 	else:
 		nx.draw_spring(nx.subgraph_view(real_graph_list[i],filter_node=cfunction),node_color=graph_colors,with_labels=True,node_size=500,font_size=10)
-		
 	plt.draw()
 	figManager = plt.get_current_fig_manager()
-	#figManager.window.showMaximized() #erro no linux
+	figManager.full_screen_toggle()
 	l,r = plt.xlim()
-	print(l,r)
+	#print(l,r)
 	plt.xlim(l-2,r+2)
 	plt.show()
 	i+=1
